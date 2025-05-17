bfb8d6c2d526c5a93727bb828d778c18470020cf
DevOjeda016 <172947644+DevOjeda016@users.noreply.github.com>
2025-05-16 19:08:43 -0600

    Add initial server setup with Express and routing

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import apiAuth from './modules/auth/index.js';
+import db from './db/index.js';
+import userRouter from './modules/users/index.js';
+
+const app = express();
+
+db.connectToDb();
+
+app.use(express.json());
+app.get('/', (req, res) => res.send('Hello world'));
+app.use('/api/auth', apiAuth);
+app.use('/api/users', userRouter);
+
+export default app;
